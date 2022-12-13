8457ca8ed010b343c6ec584ac5f38bd9df8aae61
frosthyperion <sahilnarwal1032@gmail.com>
2022-12-13 09:44:32 -0600

    landing page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,35 @@
-import React from "react";
-import "./Home.css";
+import React, { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 
-const Home = () => {
-  return <div className="home">Home</div>;
-};
+import {
+  HeroContainer,
+  HeroContent,
+  HeroItems,
+  HeroH1,
+  HeroP,
+  HeroBtn,
+} from "./HomeElement";
 
-export default Home;
+function Hero() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <HeroContainer>
+      <HeroContent>
+        <HeroItems>
+          <HeroH1>Greatest Donut Ever</HeroH1>
+          <HeroP>Ready in 99 seconds</HeroP>
+          <Link to="/donuts">
+            <HeroBtn>Place Order</HeroBtn>
+          </Link>
+        </HeroItems>
+      </HeroContent>
+    </HeroContainer>
+  );
+}
+
+export default Hero;
